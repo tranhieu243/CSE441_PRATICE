12864d0df87fd5b6bad9956176a8c65a3f0f7d23
tranhieu243 <ht02042003@gmail.com>
2024-10-17 00:54:44 +0700

    update Ex07

--- Ex07/app/src/main/java/com/example/ex07/ChildActivity.java
+++ Ex07/app/src/main/java/com/example/ex07/ChildActivity.java
@@ -1,4 +1,25 @@
 package com.example.ex07;
 
-public class ChildActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChildActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_child); // Sửa lỗi này
+
+        Button btn = (Button) findViewById(R.id.btn_backToMain);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(intent1); // Bổ sung lệnh startActivity để chuyển sang MainActivity
+            }
+        });
+    }
 }
--- Ex07/app/src/main/java/com/example/ex07/MainActivity.java
+++ Ex07/app/src/main/java/com/example/ex07/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex07;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_changeToChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn_changeToChild = findViewById(R.id.btn_changeToChild);
+        btn_changeToChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, ChildActivity.class);
+                startActivity(intent1);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
