e918946a80ee0e5c013df60bfa48c1aa06d5b29c
tranhieu243 <ht02042003@gmail.com>
2024-10-19 19:12:16 +0700

    Ex10

--- Ex10/app/src/main/java/com/example/ex10/MySmsReceive.java
+++ Ex10/app/src/main/java/com/example/ex10/MySmsReceive.java
@@ -1,4 +1,35 @@
 package com.example.ex10;
 
-public class MySmsReceive {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class MySmsReceive extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        processReceive(context, intent);
+    }
+
+    private void processReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        String message = "";
+        String body = "";
+        String address = "";
+        if (extras != null) {
+            Object[] smsEtra = (Object[]) extras.get("pdus");
+            for (int i = 0; i < smsEtra.length;i++){
+                SmsMessage sms = SmsMessage.createFromPdu((byte[])smsEtra[i]);
+                body = sms.getMessageBody();
+                address = sms.getOriginatingAddress();
+                message += "Có 1 tin nhắn từ"+address+"\n"+body+" vừa gửi đến";
+            }
+            Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+        }
+    }
 }
