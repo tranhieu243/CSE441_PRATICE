d455079b99bdd89378271e0d01c4be0181b60598
tranhieu243 <ht02042003@gmail.com>
2024-10-20 21:58:01 +0700

    Ex12

--- Ex12/app/src/main/java/com/example/ex12/MainActivity.java
+++ Ex12/app/src/main/java/com/example/ex12/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex12;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tx1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        tx1 = findViewById(R.id.selection);
+        final String arr1[] = {"Iphone 14promax", "Samsung Galaxy S24ultra", "Nokia Lumia 730", "Sony Xperia XZ", "HTC One E9"};
+        Object ArrayAdapter;
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arr1);
+        ListView lv1 = findViewById(R.id.lv1);
+        lv1.setAdapter(adapter1);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                tx1.setText("Điện thoại được chọn "+i+" : "+arr1[i]);
+            }
         });
     }
 }
