7f0b1f10ec10b7f774f0514e43adeb59b8a3ab1c
tranhieu243 <ht02042003@gmail.com>
2024-10-20 23:27:20 +0700

    Ex13.2

--- Ex13.2/app/src/main/java/com/example/ex132/MainActivity.java
+++ Ex13.2/app/src/main/java/com/example/ex132/MainActivity.java
@@ -1,6 +1,12 @@
+
 package com.example.ex132;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] arr = {"B-Phone","Apple","Xiaomi","Samsung","Nokia","Sony","LG","Oppo","HTC","V-Smart","Huawei","Vivo"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        final TextView selection = (TextView) findViewById(R.id.selection);
+        final GridView gv = (GridView) findViewById(R.id.gridView1);
+        ArrayAdapter<String>da = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        gv.setAdapter(da);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selection.setText(arr[i]);
+            }
         });
     }
 }
