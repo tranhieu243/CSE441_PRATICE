09fa8fcf21d96e272de0d7eedb5ae5d14803bd92
tranhieu243 <ht02042003@gmail.com>
2024-10-20 23:01:08 +0700

    Ex12.3

--- Ex12.3/app/src/main/java/com/example/ex123/MainActivity.java
+++ Ex12.3/app/src/main/java/com/example/ex123/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex123;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] arr1 = {"Hàng điện tử ", "Hàng hóa chất", "Hàng gia dụng", "Hàng xây dựng"};
+    TextView txt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txt1 = (TextView) findViewById(R.id.txt1);
+        Spinner spin1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
+                MainActivity.this, android.R.layout.simple_spinner_item, arr1
+        );
+        adapter1.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spin1.setAdapter(adapter1);
+        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                txt1.setText(arr1[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
     }
 }
