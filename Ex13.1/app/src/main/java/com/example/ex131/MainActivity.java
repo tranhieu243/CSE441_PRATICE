9ec3f81369bcdd3c2fe609597fd5f63686501e6e
tranhieu243 <ht02042003@gmail.com>
2024-10-20 23:17:59 +0700

    Ex13.1

--- Ex13.1/app/src/main/java/com/example/ex131/MainActivity.java
+++ Ex13.1/app/src/main/java/com/example/ex131/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex131;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView selection;
+    AutoCompleteTextView singleComplete;
+    MultiAutoCompleteTextView multiComplete;
+    String[] arr = {"Hà Nội","Hạ Long","Hưng yên","Hà Giang","Đà Nẵng","Nghệ An","Quảng Bình","Quảng Nam"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        selection = (TextView) findViewById(R.id.selection);
+        singleComplete = (AutoCompleteTextView) findViewById(R.id.editauto);
+        ArrayAdapter myadapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,arr
+        );
+        singleComplete.setAdapter(myadapter);
+        multiComplete = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
+        multiComplete.setAdapter(new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,arr
+        ));
+        multiComplete.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        singleComplete.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                selection.setText(singleComplete.getText());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
         });
     }
 }
