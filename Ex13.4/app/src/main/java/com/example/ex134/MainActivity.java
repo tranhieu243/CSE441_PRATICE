3cb46f5d56b28e3f7335edfb91e806c5635dbcef
tranhieu243 <ht02042003@gmail.com>
2024-10-21 00:31:33 +0700

    Ex13.4

--- Ex13.4/app/src/main/java/com/example/ex134/MainActivity.java
+++ Ex13.4/app/src/main/java/com/example/ex134/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex134;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    public static String[] arrayName = {"Ảnh 1", "Ảnh 2", "Ảnh 3", "Ảnh 4", "Ảnh 5", "Ảnh 6"};
+    public static int[] imageName = {R.drawable.hl, R.drawable.hn, R.drawable.dn, R.drawable.h, R.drawable.qn, R.drawable.hg};
+    GridView gridViewDemo;
+    MyArrayAdapter adapterDanhSach;
+    ArrayList<Image> arrimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        gridViewDemo = (GridView) findViewById(R.id.grid1);
+        arrimage = new ArrayList<Image>();
+        adapterDanhSach = new MyArrayAdapter(MainActivity.this, R.layout.listitem, arrimage);
+        gridViewDemo.setAdapter(adapterDanhSach);
+        for (int i = 0; i < imageName.length;i++){
+            Image myimage = new Image();
+            myimage.setName(arrayName[i]);
+            myimage.setImg(imageName[i]);
+            arrimage.add(myimage);
+            adapterDanhSach.notifyDataSetChanged();
+        }
+        gridViewDemo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent1 = new Intent(MainActivity.this, SubActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("TITLE", i);
+                intent1.putExtras(bundle);
+                startActivity(intent1);
+            }
         });
     }
 }
--- Ex13.4/app/src/main/java/com/example/ex134/SubActivity.java
+++ Ex13.4/app/src/main/java/com/example/ex134/SubActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex134;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SubActivity extends AppCompatActivity {
-
+    private Bundle extra;
+    TextView txtname2;
+    ImageView img2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sub);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        txtname2 = (TextView) findViewById(R.id.textView2);
+        img2 = (ImageView) findViewById(R.id.imageView2);
+        extra = getIntent().getExtras();
+        int position = extra.getInt("TITLE");
+        txtname2.setText(MainActivity.arrayName[position]);
+        img2.setImageResource(MainActivity.imageName[position]);
     }
 }
