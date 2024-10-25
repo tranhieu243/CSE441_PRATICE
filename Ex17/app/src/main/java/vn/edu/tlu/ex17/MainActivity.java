bd199dfe8e068e0ac1d4f950f09b4dc2abdcc885
tranhieu243 <ht02042003@gmail.com>
2024-10-25 11:29:49 +0700

    Ex17

--- Ex17/app/src/main/java/vn/edu/tlu/ex17/MainActivity.java
+++ Ex17/app/src/main/java/vn/edu/tlu/ex17/MainActivity.java
@@ -2,23 +2,88 @@ package vn.edu.tlu.ex17;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    String DB_PATH_SUFFIX = "/databases/";
+    SQLiteDatabase database = null;
+    String DATABASE_NAME = "QLSach.db";
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        processCopy();
+        database = openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+
+        // Kiểm tra xem bảng có dữ liệu không
+        Cursor c = database.query("tbsach", null, null, null, null, null, null);
+        if (c.moveToFirst()) {
+            do {
+                String data = c.getString(0) + "-" + c.getString(1) + "-" + c.getString(2);
+                mylist.add(data);
+            } while (c.moveToNext());
+        } else {
+            Toast.makeText(this, "Không có dữ liệu", Toast.LENGTH_LONG).show();
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
+    }
+
+    private void processCopy() {
+        File dbFile = getDatabasePath(DATABASE_NAME);
+        if (!dbFile.exists()) {
+            try {
+                CopyDataBaseFromAsset();
+                Toast.makeText(this, "Copying success from Assets folder", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private String getDatabasePathCustom() {
+        return getApplicationInfo().dataDir + DB_PATH_SUFFIX + DATABASE_NAME;
+    }
+
+    public void CopyDataBaseFromAsset() {
+        try {
+            InputStream myInput = getAssets().open(DATABASE_NAME);
+            String outFileName = getDatabasePathCustom();
+            File f = new File(getApplicationInfo().dataDir + DB_PATH_SUFFIX);
+            if (!f.exists()) f.mkdir();
+
+            OutputStream myOutput = new FileOutputStream(outFileName);
+            byte[] buffer = new byte[1024];  // Đọc dữ liệu từng khối 1KB
+            int length;
+            while ((length = myInput.read(buffer)) > 0) {
+                myOutput.write(buffer, 0, length);
+            }
+
+            myOutput.flush();
+            myOutput.close();
+            myInput.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
