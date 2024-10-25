9394ba926dc7a62c7e218fd60a2a92137f7007d7
tranhieu243 <ht02042003@gmail.com>
2024-10-25 12:16:05 +0700

    Ex20

--- Ex20/app/src/main/java/vn/edu/tlu/ex20/MainActivity.java
+++ Ex20/app/src/main/java/vn/edu/tlu/ex20/MainActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.tlu.ex20;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStart;
+    MyAsyncTask mytt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnStart = (Button) findViewById(R.id.button1);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doStart();
+            }
         });
+    }
+    public void doStart(){
+        mytt = new MyAsyncTask(this);
+        mytt.execute();
     }
 }
