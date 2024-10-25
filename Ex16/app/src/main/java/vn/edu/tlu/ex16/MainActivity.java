3990480b1b7dd9a0d7e627bf11f0b5ebc49df02c
tranhieu243 <ht02042003@gmail.com>
2024-10-25 10:19:36 +0700

    layout Ex16

--- Ex16/app/src/main/java/vn/edu/tlu/ex16/MainActivity.java
+++ Ex16/app/src/main/java/vn/edu/tlu/ex16/MainActivity.java
@@ -1,6 +1,11 @@
 package vn.edu.tlu.ex16;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,50 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtA, edtB, edtKQ;
+    Button btnTong, btnClear;
+    TextView txtLichSu;
+    String lichsu = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btnTong = findViewById(R.id.btnTong);
+        btnClear = findViewById(R.id.btnClear);
+        txtLichSu = findViewById(R.id.txtLichSu);
+        SharedPreferences myprefs = getSharedPreferences("mysave", MODE_PRIVATE);
+        lichsu = myprefs.getString("ls","");
+        txtLichSu.setText(lichsu);
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                int kq = a + b;
+                edtKQ.setText(kq+"");
+                lichsu += a+" + "+b+" = "+kq;
+                txtLichSu.setText(lichsu);
+                lichsu += "\n";
+            }
         });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lichsu="";
+                txtLichSu.setText(lichsu);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences myprefs = getSharedPreferences("mysave", MODE_PRIVATE);
+        SharedPreferences.Editor myedit = myprefs.edit();
+        myedit.putString("ls", lichsu);
+        myedit.commit();
     }
 }
