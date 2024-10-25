7cf26b17e887c97768e273ad2d7c18709b025a6d
tranhieu243 <ht02042003@gmail.com>
2024-10-25 12:27:22 +0700

    Ex22

---
+++ Ex22/app/src/main/java/vn/edu/tlu/ex22/XMLParser.java
@@ -0,0 +1,52 @@
+package vn.edu.tlu.ex22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class XMLParser {
+    public String getXMLFromUrl(String urlString) {
+        String xml = null;
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        try {
+            URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuilder stringBuilder = new StringBuilder();
+            if (inputStream == null) {
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            if (stringBuilder.length() == 0) {
+                return null;
+            }
+            xml = stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return xml;
+    }
+}
